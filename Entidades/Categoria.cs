673ed0c3edaede6589e274029767362086babcc1
Javier Lete <javier@eskura21.com>
2021-09-21 09:16:27 +0200

    Entidades

---
+++ Entidades/Categoria.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Categoria : IEquatable<Categoria>
+    {
+        public long? Id { get; set; }
+        public string Nombre { get; set; }
+        public IEnumerable<Producto> Productos { get; set; }
+
+        public Categoria(long? id, string nombre)
+        {
+            Id = id;
+            Nombre = nombre;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Categoria);
+        }
+
+        public bool Equals(Categoria other)
+        {
+            return other != null &&
+                   Id == other.Id &&
+                   Nombre == other.Nombre &&
+                   EqualityComparer<IEnumerable<Producto>>.Default.Equals(Productos, other.Productos);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1546776674;
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nombre);
+            hashCode = hashCode * -1521134295 + EqualityComparer<IEnumerable<Producto>>.Default.GetHashCode(Productos);
+            return hashCode;
+        }
+
+        public static bool operator ==(Categoria left, Categoria right)
+        {
+            return EqualityComparer<Categoria>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Categoria left, Categoria right)
+        {
+            return !(left == right);
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Nombre}";
+        }
+    }
+}
---
+++ Entidades/Producto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Producto : IEquatable<Producto>
+    {
+        public long? Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public Categoria Categoria { get; set; }
+
+        public Producto(long? id, string nombre, decimal precio, Categoria categoria)
+        {
+            Id = id;
+            Nombre = nombre;
+            Precio = precio;
+            Categoria = categoria;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Producto);
+        }
+
+        public bool Equals(Producto other)
+        {
+            return other != null &&
+                   Id == other.Id &&
+                   Nombre == other.Nombre &&
+                   Precio == other.Precio &&
+                   EqualityComparer<Categoria>.Default.Equals(Categoria, other.Categoria);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1876611217;
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nombre);
+            hashCode = hashCode * -1521134295 + Precio.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<Categoria>.Default.GetHashCode(Categoria);
+            return hashCode;
+        }
+
+        public static bool operator ==(Producto left, Producto right)
+        {
+            return EqualityComparer<Producto>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Producto left, Producto right)
+        {
+            return !(left == right);
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Nombre}, {Precio}, {Categoria}";
+        }
+    }
+}
