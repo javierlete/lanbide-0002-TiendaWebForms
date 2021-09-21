0f5aa56b7d77270f61378d8195ab43fe69c6cc31
Javier Lete <javier@eskura21.com>
2021-09-21 09:39:41 +0200

    Daos con obtener categorías

---
+++ Daos/DaoSqlServerCategoria.cs
@@ -0,0 +1,76 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daos
+{
+    public class DaoSqlServerCategoria : IDao<Categoria>
+    {
+        private const string CADENA_CONEXION = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=mf0966;Integrated Security=True";
+        private const string SQL_SELECT = @"SELECT * FROM Categorias";
+
+        #region Singleton
+        private DaoSqlServerCategoria() { }
+
+        private static DaoSqlServerCategoria dao = new DaoSqlServerCategoria();
+
+        public static DaoSqlServerCategoria ObtenerDao()
+        {
+            return dao;
+        }
+
+        #endregion
+        
+        private static SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(CADENA_CONEXION);
+        }
+
+        public IEnumerable<Categoria> ObtenerTodos()
+        {
+            using (IDbConnection con = ObtenerConexion())
+            {
+                IDbCommand com = con.CreateCommand();
+
+                com.CommandText = SQL_SELECT;
+
+                IDataReader dr = com.ExecuteReader();
+
+                List<Categoria> categorias = new List<Categoria>();
+
+                while(dr.Read())
+                {
+                    categorias.Add(new Categoria((long)dr["Id"], (string)dr["Nombre"]));
+                }
+
+                return categorias;
+            }
+        }
+
+
+        public Categoria ObtenerPorId(long id)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public Categoria Insertar(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Categoria Modificar(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Borrar(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Daos/IDao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daos
+{
+    public interface IDao<T>
+    {
+        IEnumerable<T> ObtenerTodos();
+        T ObtenerPorId(long id);
+        T Insertar(T objeto);
+        T Modificar(T objeto);
+        void Borrar(long id);
+    }
+}
